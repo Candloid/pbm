51b3ce0ff418c87057c2695b2af530ca45c8381a
betonenbomber <vmelnikov.miis@gmail.com>
2019-03-14 17:26:14 -0400

    "Removed commented unnecessary code in Main"

--- src/com/concordia/personalBudgetManager/Main.java
+++ src/com/concordia/personalBudgetManager/Main.java
@@ -8,21 +8,7 @@ import com.concordia.personalBudgetManager.ExpenseRecord.repetitionIntervalE;
 
 public class Main {
     public static void main(String[] args) {
-	// write your code here
     	User currentUser = new User("Jane Doe");
-    	
-    	/*
-    	//loading expense records
-		ExpenseRecord [] preloadedRecords = new ExpenseRecord [5];
-		for (int i = 0; i < preloadedRecords.length; i++) {
-			preloadedRecords[i] = new ExpenseRecord(1000 * i + 1, false, LocalDate.now(), expenseTypeE.Purchase,
-					paymentTypeE.dueByCredit, repetitionIntervalE.Once, "Retailer " + i, "Location " + i,
-					LocalDate.now(), "");
-			currentUser.records.add(preloadedRecords[i]);
-		}
-		//end of loading expense records
-		*/
-		
 		RecordManagementForm editorUserForm = new RecordManagementForm(currentUser);
     	editorUserForm.loadForm(currentUser);
     }
